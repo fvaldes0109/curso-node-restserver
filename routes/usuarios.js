8932ed68ac34a73c82a52d49faaac917be935421
FernanValdes <fvaldes0109@gmail.com>
2021-08-06 20:08:10 -0400

    Usuarios CRUD listo

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,15 +1,35 @@
 const {Router} = require('express');
+const {check} = require('express-validator');
+
 const {usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch} = require('../controllers/usuarios');
+const {esRoleValido, existeEmail, existeUsuarioPorId} = require('../helpers/db-validators');
+const {validarCampos} = require('../middlewares/validar-campos')
 
 const router = new Router();
 
 router.get('/', usuariosGet);
 
-router.put('/:id', usuariosPut);
+router.put('/:id', [
+    check('id', 'No es un ID valido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    check('rol').custom(esRoleValido),
+    validarCampos
+], usuariosPut);
 
-router.post('/', usuariosPost);
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe tener mas de 6 letras').isLength({min: 6}),
+    check('correo', 'El correo no es valido').isEmail(),
+    check('correo').custom(existeEmail),
+    check('rol').custom(esRoleValido), // rol => esRolValido(rol)
+    validarCampos
+], usuariosPost);
 
-router.delete('/', usuariosDelete);
+router.delete('/:id', [
+    check('id', 'No es un ID valido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    validarCampos
+], usuariosDelete);
 
 router.patch('/', usuariosPatch);
 
